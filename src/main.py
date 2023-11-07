b1d3c64ff73064d9c3758910fb109ffc2ee9b60b
Irina Klepova <ivklepova23@gmail.com>
2023-11-07 05:25:26 +0400

    реализована точка входа

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from user_interface import job_search
+# from src.hh_api import HeadHunterAPI
+# from src.sj_api import SuperJobAPI
+# from src.vacancy import Vacancy
+# from src.json_saver import JsonFile
+
+if __name__ == "__main__":
+    job_search()
