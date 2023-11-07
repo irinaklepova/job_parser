0bd46fe036e52395c54015e9bef4734e5c7e2752
Irina Klepova <ivklepova23@gmail.com>
2023-11-07 05:20:49 +0400

    реализован класс для работы с HH API

---
+++ src/hh_api.py
@@ -0,0 +1,54 @@
+from abstract_classes import AbsAPI
+import requests
+from vacancy import Vacancy
+
+
+class HeadHunterAPI(AbsAPI):
+    area_url_api = "https://api.hh.ru/areas"
+    vacancy_url_api = "https://api.hh.ru/vacancies"
+
+    def __init__(self, keyword: str):
+        self.params = {
+            'per_page': 100,
+            'text': keyword,
+        }
+        self.keyword = keyword
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.params['per_page']}', {self.params['text']})"
+
+    def get_vacancies(self) -> list[dict]:
+        vacancies_list = []
+        response = requests.get(self.vacancy_url_api, self.params)
+        data = response.json()
+        vacancies_list.extend(data.get("items"))
+        return vacancies_list
+
+    def formate_vacancies(self, all_vacancies: list) -> list:
+        vacancies_hh = []
+        for item in all_vacancies:
+            if item['salary'] is None:
+                salary = "З/п не указана"
+            elif item['salary']['from'] is None:
+                salary = item['salary']['to']
+            elif item['salary']['to'] is None:
+                salary = item['salary']['from']
+            else:
+                salary = (int(item['salary']['from']) + int(item['salary']['to'])) // 2
+            vac = {'name': item['name'], 'area': item['area']['name'], 'salary': salary,
+                   'experience': item['experience']['name'], 'url': item['alternate_url']}
+            vacancies_hh.append(Vacancy(vac))
+        return vacancies_hh
+
+    # def get_areas(self):
+    #     req = requests.get(HeadHunterAPI.area_url_api)
+    #     dict_areas = req.json()
+    #     return dict_areas
+
+# hh = HeadHunterAPI("инженер")
+# li = hh.get_vacancies()
+# print(hh.formate_vacancies(li))
+# for i in hh.formate_vacancies(li):
+#     print(i)
+# # for i in hh.get_areas():
+# #     print(i)
