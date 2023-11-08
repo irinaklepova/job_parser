5f8ad86ecdfe8ff455e66a55600a53419d4d2263
Irina Klepova <ivklepova23@gmail.com>
2023-11-08 23:27:14 +0400

    доработана возможность сохранения результата, удален избыточный код

--- src/user_interface.py
+++ src/user_interface.py
@@ -5,24 +5,32 @@ from json_saver import JsonFile
 
 
 def view_result(result: list, count_view: str):
-    for res in result:
-        if count_view == '1':
+    """Функция для выбора вывода результата"""
+
+    if count_view == '1':
+        for res in result:
             print(res)
-        elif count_view == '2':
-            jf = JsonFile()
-            jf.add_vacancy(res)
-            print("Готово!")
+
+    elif count_view == '2':
+        jf.save_to_file(Vacancy.vacancy_to_json(result))
+
+        print("Готово!")
 
 
 def job_search():
+    """Функция для взаимодействия с пользователем"""
 
+    jf.delete_file()
+
+    api = None
     user_name = input("Здравствуйте, введите Ваше имя ").title()
 
-    while True:
+    while not api:
         place = input(f"{user_name}, выберите, на какой площадке будем искать вакансии (введите номер)\n"
                       f"1 - HeadHunter\n"
                       f"2 - SuperJob\n"
                       f"0 - выход\n")
+
         if place == '0':
             print("До свидания!")
             quit()
@@ -32,17 +40,16 @@ def job_search():
             move_vac = input(f"{user_name}, выберите, что делать с результатом поиска:\n"
                              f"1 - вывести на экран\n"
                              f"2 - записать в файл\n")
+
             if place == '1':
-
-                hh_api = HeadHunterAPI(search_text)
-                vac_list = hh_api.get_vacancies()
-                all_vacancies = hh_api.formate_vacancies(vac_list)
-                result = Vacancy.output_final_result(all_vacancies, count_view)
-                view_result(result, move_vac)
-
+                api = HeadHunterAPI(search_text)
             elif place == '2':
-                sj_api = SuperJobAPI(search_text)
-                vac_list = sj_api.get_vacancies()
-                all_vacancies = sj_api.formate_vacancies(vac_list)
-                result = Vacancy.output_final_result(all_vacancies, count_view)
-                view_result(result, move_vac)
+                api = SuperJobAPI(search_text)
+
+            vac_list = api.get_vacancies()
+            all_vacancies = api.formate_vacancies(vac_list)
+            result = Vacancy.output_final_result(all_vacancies, count_view)
+            view_result(result, move_vac)
+
+
+jf = JsonFile()
